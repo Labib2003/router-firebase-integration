211c562008ea4ec9defcb687babaed48881eff09
Labib2003 <labib.salimi30@gmail.com>
2022-04-09 14:15:49 +0600

    routes, components and custom hook created

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,16 @@ const Login = () => {
     return (
         <div>
             <h2>This is login</h2>
+            <div style={{margin: '20px'}}>
+                <button>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
