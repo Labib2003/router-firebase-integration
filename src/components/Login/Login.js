110827f151ffc10fd1fa4f07326aae122c7b57e6
Labib2003 <labib.salimi30@gmail.com>
2022-04-09 15:38:52 +0600

    used react firebase hooks to manage user auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,17 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
-    
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
     return (
         <div>
             <h2>This is login</h2>
-            <div style={{margin: '20px'}}>
-                <button onClick={}>Google Sign In</button>
+            <div style={{ margin: '20px' }}>
+                <button onClick={() => signInWithGoogle()}>Google Sign In</button>
             </div>
             <form>
                 <input type="email" placeholder='Email' />
